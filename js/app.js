1705a6d4b5fe9612c9de0a75d913b8ab5dd76100
josephbrockw <joew@theroasterie.com>
2015-03-17 22:33:25 -0500

    Completed Hot or Cold Basic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 
 $(document).ready(function(){
-	
+	/*--- Global Variables ---*/
+	var secret;
+	var guess;
+	/*--- Automatically Start New Game ---*/
+	function newGame() {
+		secret = Math.floor(Math.random()*100+1);
+  		$('#count').text('0');
+  		$('.guessBox').empty();
+  		$('#feedback').text('Make your Guess!')
+  		console.log(secret);
+	}
+
+	newGame();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +25,48 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- Start New Game ---*/
+  	$('a.new').click(function() {
+  		newGame();
+  		console.log(secret);
+  	});
+
+  	/*--- Hot or Cold the Game ---*/
+  	$('#guessButton').click(function() {
+  		/*--- Validation ---*/
+  		// Still need to prevent NaN
+  		if($('#userGuess').val()<1) {
+  			alert('Please pick a number between 1 and 100.');
+  		} else if($('#userGuess').val()>100){
+  			alert('Please pick a number between 1 and 100.');
+  		} else {
+  			guess = $('#userGuess').val();
+  			$('.guessBox').append('<li>' + $('#userGuess').val() + '</li>');
+  			$('#count').text(parseInt($('#count').text())+1);
+  			$('#userGuess').val('');
+  			
+
+  			/*--- Feedback ---*/
+  			if (Math.abs(secret-guess)==0) {
+  				$('#feedback').text('You Win!');
+  				newGame();
+  			} else if(1<Math.abs(secret-guess)<=10) {
+  				$('#feedback').text('Scortchin\' Hot');
+  			} else if(10<Math.abs(secret-guess)<=20) {
+  				$('#feedback').text('Hot');
+  			} else if(20<Math.abs(secret-guess)<=30) {
+  				$('#feedback').text('Warm');
+  			} else if(30<Math.abs(secret-guess)<=50) {
+  				$('#feedback').text('Cold');
+  			} else {
+  				$('#feedback').text('Freezing');
+  			}
+  		} 		
+  	});
+
+  	$('form').submit(function(event) {
+  		event.preventDefault();
+  	});
 });
 
 
